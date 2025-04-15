c261d8d5180197d7985958704294d6d44dfbe953
Anjishnu Dey <anjishnu.04032007@gmail.com>
2025-04-15 11:08:28 +0530

    leadership bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,10 @@ import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { ArrowUpRight, Award, CalendarCheck, PaintBucket, UserCheck } from "lucide-react"
 import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import "./globals.css";
+import { Footer } from "@/components/footer"
+
 
 export default function Home() {
   return (
@@ -25,7 +26,7 @@ export default function Home() {
         width={490}
         height={560}
         quality={100}
-        className=" object-cover w-full h-full absolute -top-20 left-0 -z-10 lg:hidden"
+        className="object-cover w-full h-full absolute -top-20 left-0 -z-10 lg:hidden lg:"
         style={{
           maskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 90%)",
           WebkitMaskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%)",
@@ -62,7 +63,7 @@ export default function Home() {
             width={385}
             height={492}
             quality={100}
-            className="object-cover absolute top-0 bottom-0 my-auto right-0 left-17 mr-auto hidden dark:inline"
+            className="object-cover absolute top-0 bottom-0 my-auto right-0 left-17 mr-auto hidden dark:inline "
           />
           <Image
             src="/ribbon_light.png"
@@ -328,80 +329,7 @@ export default function Home() {
 
 
       {/* Footer */}
-      <div className="flex flex-col items-start bg-accent justify-start w-full h-fit lg:px-14 md:px-10 px-6 py-10 sm:py-14 md:py-18 lg:py-20">
-        <div className="flex flex-row w-full h-fit justify-around lg:px-14 md:px-10 px-6 pb-8  gap-10">
-          <div className="flex flex-col items-start gap-4 lg:max-w-[40%]">
-            <p className="text-lg font-giest text-foreground">Get notified when we publish.</p>
-            <div className="flex flex-row gap-2 w-full">
-              <Input type="email" placeholder="Email" className="py-5" />
-              <Button className="py-5">Submit</Button>
-            </div>
-            <p className="text-foreground font-giest">We respect <u>your privacy</u> and promise not to spam. Unsubscribe anytime.</p>
-          </div>
-          <div className="lg:flex hidden flex-col items-start gap-6 text-lg font-giest text-foreground lg:max-w-[20%]">
-            <h1 className="font-bold">Contact</h1>
-            <div>
-              <div className="flex flex-col gap-4">
-                <Link href="#">hello@alfarex.me</Link>
-                <Link href="#">+91 760 505 5424</Link>
-                <p>The Carcino Foundation Signature Towers III - Tower A, Sector 15 Part 2, Village Silokhera, Gurugram, Haryana 122002</p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:flex hidden flex-col items-start gap-6 text-lg font-giest text-foreground lg:max-w-[20%]">
-            <h1 className="font-bold">Pages</h1>
-            <div className="flex flex-col gap-4">
-              <Link href="#">Home</Link>
-              <Link href="#">About</Link>
-              <Link href="#">Research</Link>
-              <Link href="#">Articles</Link>
-              <Link href="#">Blog</Link>
-            </div>
-          </div>
-          <div className="lg:flex hidden flex-col items-start gap-6 text-lg font-giest text-foreground lg:max-w-[20%]">
-            <h1 className="font-bold">Social</h1>
-            <div className="flex flex-col gap-4">
-              <Link href="#">X</Link>
-              <Link href="#">LinkedIn</Link>
-              <Link href="#">Instagram</Link>
-              <Link href="#">Youtube</Link>
-              <Link href="#">Facebook</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex lg:hidden flex-row items-start w-full h-fit justify-center text-sm font-giest text-foreground lg:px-14 md:px-10 px-6 sm:py-14 md:py-18 lg:py-20 gap-10">
-          <div className="flex flex-col items-start gap-3">
-            <h1 className="font-bold">Contact</h1>
-            <div className="flex flex-col gap-1">
-              <Link href="#">hello@alfarex.me</Link>
-              <Link href="#">+91 760 505 5424</Link>
-              <p>The Carcino Foundation Signature Towers III - Tower A, Sector 15 Part 2, Village Silokhera, Gurugram, Haryana 122002</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-start gap-3">
-            <h1 className="font-bold">Pages</h1>
-            <div className="flex flex-col gap-1">
-              <Link href="#">Home</Link>
-              <Link href="#">About</Link>
-              <Link href="#">Research</Link>
-              <Link href="#">Articles</Link>
-              <Link href="#">Blog</Link>
-            </div>
-          </div>
-          <div className="flex flex-col items-start gap-3">
-            <h1 className="font-bold">Social</h1>
-            <div className="flex flex-col gap-1">
-              <Link href="#">X</Link>
-              <Link href="#">LinkedIn</Link>
-              <Link href="#">Instagram</Link>
-              <Link href="#">Youtube</Link>
-              <Link href="#">Facebook</Link>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <Footer></Footer>
 
     </div>
   );
