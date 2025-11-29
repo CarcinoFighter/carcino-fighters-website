df13b31de87b6fc700345751d8cfd9be6972cfe6
Naitik Chattaraj <naitikchattaraj41@gmail.com>
2025-11-29 16:24:16 +0530

    Button now has glass

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,45 +103,76 @@ export default function RootLayout({
           
           <Navbar></Navbar>
          {/* Global hidden SVG filter */}
-<svg style={{ display: "none" }}>
-  <filter
-    id="glass-distortion"
-    x="0"
-    y="0"
-    width="100%"
-    height="100%"
-    filterUnits="objectBoundingBox"
-  >
-
-    <feTurbulence
-      type="fractalNoise"
-      baseFrequency="0.007 0.01"
-      numOctaves="3"
-      seed="2"
-      result="turb"
-    />
-
-    <feGaussianBlur in="turb" stdDeviation="2.2" result="blurredNoise" />
-
-    <feSpecularLighting
-      in="blurredNoise"
-      surfaceScale="2"
-      specularConstant="0.6"
-      specularExponent="40"
-      lightingColor="#ffffff"
-      result="light"
+<svg width={0} height={0} style={{ position: "absolute" }}>
+  <defs>
+    <filter
+      id="nav-glass"
+      x="-20%"
+      y="-20%"
+      width="140%"
+      height="140%"
+      filterUnits="objectBoundingBox"
     >
-      <fePointLight x="-80" y="-40" z="120" />
-    </feSpecularLighting>
-    <feDisplacementMap
-      in="SourceGraphic"
-      in2="blurredNoise"
-      scale="20"
-      xChannelSelector="R"
-      yChannelSelector="G"
-    />
-  </filter>
+      {/* Horizontal refraction bias */}
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency="0.002 0.05"
+        numOctaves="3"
+        seed="7"
+        result="warpNoise"
+      />
+
+      {/* Stretch distortion stronger in middle, weaker edges */}
+      <feComponentTransfer in="warpNoise" result="centerWarp">
+        <feFuncR type="gamma" exponent="2.4" amplitude="1" offset="0" />
+        <feFuncG type="gamma" exponent="1.8" amplitude="1" offset="0" />
+        <feFuncB type="gamma" exponent="2.4" amplitude="1" offset="0" />
+      </feComponentTransfer>
+
+      {/* Refraction */}
+      <feDisplacementMap
+        in="SourceGraphic"
+        in2="centerWarp"
+        scale="30"
+        xChannelSelector="R"
+        yChannelSelector="G"
+        result="refracted"
+      />
+
+      {/* Frosted blur */}
+      <feGaussianBlur in="refracted" stdDeviation="10" result="frost" />
+
+      {/* Slight brightness lift like real glass */}
+      <feColorMatrix
+        in="frost"
+        type="matrix"
+        values="
+          1 0 0 0 0
+          0 1 0 0 0
+          0 0 1 0 0
+          0 0 0 .28 0
+        "
+        result="glass"
+      />
+
+      {/* Edge highlight */}
+      <feSpecularLighting
+        in="centerWarp"
+        specularConstant="1.3"
+        specularExponent="70"
+        surfaceScale="4"
+        lightingColor="#ffffff"
+        result="shine"
+      >
+        <fePointLight x="0" y="-200" z="150" />
+      </feSpecularLighting>
+
+      {/* Add rim glow */}
+      <feBlend in="glass" in2="shine" mode="screen" />
+    </filter>
+  </defs>
 </svg>
+
 
           {children}
 
