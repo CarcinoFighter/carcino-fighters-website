25bac5ed4b6ab2977d667dc6d21e31d0bd9ba015
RizDas <dkkrishabh@gmail.com>
2026-02-20 19:38:35 +0530

    minor fixes

--- app/internship/writer/page.tsx
+++ app/internship/writer/page.tsx
@@ -86,7 +86,7 @@ export default function RegistrationPage() {
         >
           Become a Writer For Us
         </h1>
-        <p className="text-sm sm:text-xl font-[100] text-white font-dmsans tracking-[120%] max-w-[33%] text-center">
+        <p className="text-sm sm:text-xl font-[100] text-white font-dmsans max-w-[50%] text-center sm:max-w-[33%]">
           Become part of our growing community working to educate the world
           about cancer prevention and awareness.
         </p>
