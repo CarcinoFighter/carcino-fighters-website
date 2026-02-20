bcc06951cf37ead8dac8002240fcb0797ffc7a82
RizDas <dkkrishabh@gmail.com>
2026-02-20 22:24:49 +0530

    tribute update

--- app/tribute/tribute.ts
+++ app/tribute/tribute.ts
@@ -8,34 +8,34 @@ export interface Tribute {
 
 // data for tribute page
 export const tributes: Tribute[] = [
-  {
-    name: "John Doe",
-    year: "1950–2020",
-    text: "Beloved community leader who dedicated his life to helping others.",
-    image: "/tribute/johndoe.png",
-  },
+  // {
+  //   name: "John Doe",
+  //   year: "1950–2020",
+  //   text: "Beloved community leader who dedicated his life to helping others.",
+  //   image: "/tribute/johndoe.png",
+  // },
   {
     name: "BARSHA CHAKRABORTY",
-    year: "30.04.1982 – 28.03.2018",
+    year: "30/04/1982 – 28/03/2018",
     text: "",
     image: "/tribute/barshachakraborty.jpeg",
   },
   {
-    name: "Alan Smithee",
-    year: "1970–2022",
-    text: "Devoted mentor and friend, always willing to lend an ear.",
-    image: "/tribute/alansmithee.png",
+    name: "Nilima Kumar",
+    year: "11/11/1968 – 14/08/2012",
+    text: "",
+    image: "/tribute/nilimakumar.png",
   },
-  {
-    name: "Jane Smith",
-    year: "1965–2021",
-    text: "A constant source of inspiration, kindness, and wisdom.",
-    image: "/tribute/janesmith.png",
-  },
-  {
-    name: "Jane Foster",
-    year: "1965–2021",
-    text: "A constant source of inspiration, kindness, and wisdom.",
-    image: "/tribute/janefoster.png",
-  },
+  // {
+  //   name: "Jane Smith",
+  //   year: "1965–2021",
+  //   text: "A constant source of inspiration, kindness, and wisdom.",
+  //   image: "/tribute/janesmith.png",
+  // },
+  // {
+  //   name: "Jane Foster",
+  //   year: "1965–2021",
+  //   text: "A constant source of inspiration, kindness, and wisdom.",
+  //   image: "/tribute/janefoster.png",
+  // },
 ];
